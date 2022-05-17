f5beb4f30ad7402f84fac0fb972096c8cd462e54
a-girl-who-codes <beatrixlinkoln@gmail.com>
2022-05-17 23:53:23 +0300

    Update styles

--- src/components/TasksDropSelect.jsx
+++ src/components/TasksDropSelect.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import styled from "styled-components";
+
+const Select = styled.select`
+  width: 100%;
+`;
 
 export default ({tasks, onCurrentTaskChange, currentTaskId}) => {
     const total = tasks.map((task) => <option key={task.id} value={task.id}>{task.taskName}</option>)
     return (
-        <select name="tasks" id="tasksDropdown" onChange={onCurrentTaskChange} value={currentTaskId}>
+        <Select name="tasks" id="tasksDropdown" onChange={onCurrentTaskChange} value={currentTaskId}>
             {total}
-        </select>
+        </Select>
     )
 }
